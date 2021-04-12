a2901d2ef9590f456bc46ad5c28d3887e89dc02a
ubiZOX <zdh6zox@163.com>
2021-04-12 23:40:25 +0800

    terrain 20210412

---
+++ RougelikeWuXia/Source/RougelikeWuXia/BattleField/Terrain/TerrainEnums.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class ETerrainBasicType : uint8
+{
+	Base UMETA(DisplayName = "基础"),
+	Trajectory UMETA(DisplayName = "轨迹"),
+	//Secondary UMETA(DisplayName = "")
+	Exit UMETA(DisplayName = "出口")
+};
+
+UENUM(BlueprintType)
+enum class ETerrainSubType : uint8
+{
+	Base_Mud UMETA(DisplayName = "泥地"),
+	Base_Brick,
+	Base_Wood
+};
---
+++ RougelikeWuXia/Source/RougelikeWuXia/BattleField/Terrain/TerrainSavableData.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "TerrainSavableData.generated.h"
+
+/**
+ * 
+ */
+USTRUCT()
+struct ROUGELIKEWUXIA_API FTerrainPreset
+{
+	GENERATED_USTRUCT_BODY();
+};
+
+USTRUCT()
+struct ROUGELIKEWUXIA_API FTerrainBlockInfo
+{
+	GENERATED_USTRUCT_BODY();
+};
+
+
+UCLASS()
+class ROUGELIKEWUXIA_API UTerrainSavableData : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	
+};
